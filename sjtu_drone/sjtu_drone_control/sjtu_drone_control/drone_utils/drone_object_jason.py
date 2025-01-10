cae2d765de7ce48b397c8b5e58f918097a2e9c33
cdedood <C.J.deDood@student.tudelft.nl>
2025-01-10 10:25:08 +0100

    moven into folder

---
+++ sjtu_drone/sjtu_drone_control/sjtu_drone_control/drone_utils/drone_object_jason.py
@@ -0,0 +1,276 @@
+#!/usr/bin/env python3
+# (Existing License and imports remain the same)
+
+import math
+from rclpy.node import Node
+from std_msgs.msg import Empty, Bool, Int8, String
+from geometry_msgs.msg import Twist, Pose, Vector3
+from sensor_msgs.msg import Range, Image, Imu
+# Add tf_transformations to convert quaternion to Euler
+from tf_transformations import euler_from_quaternion
+
+STATES = {
+    0: "Landed",
+    1: "Flying",
+    2: "Taking off",
+    3: "Landing",
+}
+
+MODES = ["velocity", "position"]
+
+class DroneObject(Node):
+    def __init__(self, node_name: str = "drone_node", namespace: str = 'simple_drone'):
+        super().__init__(node_name=node_name, namespace=namespace)
+        self._state = STATES[0]
+        self._mode = MODES[0]
+        self._hover_distance = 0.0
+        self.isFlying = False
+        self.isPosctrl = False
+        self.isVelMode = False
+
+        self.logger = self.get_logger()
+        self.namespace = namespace
+
+        # Publishers - Construct topic names using the namespace
+        self.pubTakeOff = self.create_publisher(Empty, f'/{self.namespace}/takeoff', 1024)
+        self.pubLand = self.create_publisher(Empty, f'/{self.namespace}/land', 1024)
+        self.pubReset = self.create_publisher(Empty, f'/{self.namespace}/reset', 1024)
+        self.pubPosCtrl = self.create_publisher(Bool, f'/{self.namespace}/posctrl', 1024)
+        self.pubCmd = self.create_publisher(Twist, f'/{self.namespace}/cmd_vel', 1024)
+        self.pubVelMode = self.create_publisher(Bool, f'/{self.namespace}/dronevel_mode', 1024)
+
+        # Subscribers - Construct topic names using the namespace
+        self.sub_sonar = self.create_subscription(
+            Range, f'/{self.namespace}/sonar', self.cb_sonar, 1024)
+        self.sub_imu = self.create_subscription(
+            Imu, f'/{self.namespace}/imu', self.cb_imu, 1024)
+        self.sub_front_img = self.create_subscription(
+            Image, f'/{self.namespace}/front/image_raw', self.cb_front_img, 1024)
+        self.sub_bottom_img = self.create_subscription(
+            Image, f'/{self.namespace}/bottom/image_raw', self.cb_bottom_img, 1024)
+        self.sub_gt_pose = self.create_subscription(
+            Pose, f'/{self.namespace}/gt_pose', self.cb_gt_pose, 1024)
+        self.sub_state = self.create_subscription(
+            Int8, f'/{self.namespace}/state', self.cb_state, 1024)
+        self.sub_cmd_mode = self.create_subscription(
+            String, f'/{self.namespace}/cmd_mode', self.cb_cmd_mode, 1024)
+
+        self._sonar = Range()
+        self._imu = Imu()
+        self._front_img = Image()
+        self._bottom_img = Image()
+        self._gt_pose = Pose()
+
+        self.gt_pose_received = False
+        self.drone_spawned = False
+
+        # Wait until the drone is ready (subscribers are connected)
+        self.create_timer(1.0, self.check_subscribers)
+
+    def check_subscribers(self):
+        # Check if the drone has spawned by checking subscriber count
+        if self.pubTakeOff.get_subscription_count() > 0:
+            self.drone_spawned = True
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def mode(self):
+        return self._mode
+
+    @property
+    def hover_distance(self):
+        return self._hover_distance
+
+    @property
+    def sonar(self):
+        return self._sonar
+
+    @property
+    def imu(self):
+        return self._imu
+
+    @property
+    def front_img(self):
+        return self._front_img
+
+    @property
+    def bottom_img(self):
+        return self._bottom_img
+
+    @property
+    def gt_pose(self):
+        return self._gt_pose
+
+    @state.setter
+    def state(self, value):
+        self._state = value
+
+    @mode.setter
+    def mode(self, value):
+        self._mode = value
+
+    @hover_distance.setter
+    def hover_distance(self, value):
+        self._hover_distance = value
+
+    @sonar.setter
+    def sonar(self, value):
+        self._sonar = value
+
+    @imu.setter
+    def imu(self, value):
+        self._imu = value
+
+    @front_img.setter
+    def front_img(self, value):
+        self._front_img = value
+
+    @bottom_img.setter
+    def bottom_img(self, value):
+        self._bottom_img = value
+
+    @gt_pose.setter
+    def gt_pose(self, value):
+        self._gt_pose = value
+        self.gt_pose_received = True
+
+    def takeOff(self):
+        if self.isFlying:
+            return False
+        self.logger.info("Taking off")
+        self.pubTakeOff.publish(Empty())
+        self.isFlying = True
+        return True
+
+    def land(self):
+        if not self.isFlying:
+            return False
+        self.logger.info("Landing")
+        self.pubLand.publish(Empty())
+        self.isFlying = False
+        return True
+
+    def hover(self):
+        if not self.isFlying:
+            return False
+        twist_msg = Twist()
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def posCtrl(self, on):
+        self.isPosctrl = on
+        bool_msg = Bool()
+        bool_msg.data = on
+        self.pubPosCtrl.publish(bool_msg)
+        return True
+
+    def velMode(self, on):
+        if not self.isFlying:
+            return False
+        self.isVelMode = on
+        bool_msg = Bool()
+        bool_msg.data = on
+        self.pubVelMode.publish(bool_msg)
+        return True
+
+    def move(self, v_linear: Vector3 = Vector3(), v_angular: Vector3 = Vector3()):
+        if not self.isFlying:
+            return False
+        twist_msg = Twist(linear=v_linear, angular=v_angular)
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def moveTo(self, x: float, y: float, z: float, yaw: float = 0.0):
+        """
+        Send position + yaw setpoint in position control mode.
+        Assuming that posCtrl mode interprets cmd_vel as absolute position and yaw commands.
+        """
+        if not self.isFlying or not self.isPosctrl:
+            self.logger.error("Drone must be flying and in position control mode to moveTo a position!")
+            return False
+        twist_msg = Twist()
+        twist_msg.linear.x = x
+        twist_msg.linear.y = y
+        twist_msg.linear.z = z
+        twist_msg.angular.z = yaw
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def pitch(self, speed):
+        if not self.isFlying:
+            return False
+        twist_msg = Twist()
+        twist_msg.linear.x = 1.0
+        twist_msg.linear.y = 1.0
+        twist_msg.angular.y = speed
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def roll(self, speed: float):
+        if not self.isFlying:
+            return False
+        twist_msg = Twist()
+        twist_msg.linear.x = 1.0
+        twist_msg.linear.y = 1.0
+        twist_msg.angular.x = speed
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def rise(self, speed: float):
+        if not self.isFlying:
+            return False
+        twist_msg = Twist()
+        twist_msg.linear.z = speed
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def yaw(self, speed: float):
+        if not self.isFlying:
+            return False
+        twist_msg = Twist()
+        twist_msg.angular.z = speed
+        self.pubCmd.publish(twist_msg)
+        return True
+
+    def cb_sonar(self, msg: Range):
+        self._sonar = msg
+        self._hover_distance = msg.min_range
+
+    def cb_imu(self, msg: Imu):
+        self._imu = msg
+
+    def cb_front_img(self, msg: Image):
+        self._front_img = msg
+
+    def cb_bottom_img(self, msg: Image):
+        self._bottom_img = msg
+
+    def cb_gt_pose(self, msg: Pose) -> None:
+        self._gt_pose = msg
+        self.gt_pose_received = True
+
+    def cb_state(self, msg: Int8):
+        self._state = STATES.get(msg.data, "Unknown")
+        self.logger.info("State: {}".format(self._state), throttle_duration_sec=1)
+        # If state is Flying or Taking off, consider drone spawned
+        if msg.data in [1, 2]:  
+            self.drone_spawned = True
+
+    def cb_cmd_mode(self, msg: String):
+        if msg.data in MODES:
+            self._mode = msg.data
+            self.logger.info("Changed command mode to: {}".format(self._mode))
+        else:
+            self.logger.error("Invalid command mode: {}".format(msg.data))
+
+    def reset(self):
+        self.pubReset.publish(Empty())
+
+    def get_current_yaw(self):
+        orientation = self._gt_pose.orientation
+        quaternion = [orientation.x, orientation.y, orientation.z, orientation.w]
+        _, _, yaw = euler_from_quaternion(quaternion)
+        return yaw
---
+++ sjtu_drone/sjtu_drone_control/sjtu_drone_control/waypoint.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python3
+import math
+import rclpy
+from geometry_msgs.msg import Vector3, PoseArray
+from std_msgs.msg import String
+from drone_msgs.msg import Pose3D
+from .drone_utils.drone_object_jason import DroneObject
+import numpy as np
+
+class DronePositionControl(DroneObject):
+    def __init__(self, namespace='simple_drone'):
+        super().__init__(node_name='drone_position_control', namespace=namespace)
+        self.get_logger().info('DronePositionControl node initialized.')
+        self.position_metrics = PositionMetrics(self)
+        self.startup_flag = False
+
+        self.action_publisher = self.create_publisher(String, 'action_topic', 10)
+
+        # Listen for new waypoints
+        self.traj_subscription = self.create_subscription(
+            PoseArray,
+            '/drone_trajectory',
+            self.waypoint_callback,
+            1024
+        )
+
+        self.waypoints = [{'x': -8.0, 'y': -8.0, 'z': 2.0}, 
+                          {'x': -7.1, 'y': -6.6, 'z': 3.0}, 
+                          {'x': -4.8, 'y': -5.4, 'z': 4.0}, 
+                          {'x': -4.3, 'y': -2.9000000000000004, 'z': 2.0}, {'x': -3.5, 'y': -1.8000000000000007, 'z': 2.0}, {'x': -3.2, 'y': -1.8000000000000007, 'z': 2.0}, {'x': -1.3000000000000007, 'y': -1.0999999999999996, 'z': 2.0}, {'x': -1.1999999999999993, 'y': 1.9000000000000004, 'z': 2.0}, {'x': -0.6999999999999993, 'y': 2.5999999999999996, 'z': 2.0}, {'x': 1.0, 'y': 4.1, 'z': 2.0}, {'x': 2.1999999999999993, 'y': 5.5, 'z': 2.0}, {'x': 5.6, 'y': 5.800000000000001, 'z': 2.0}, {'x': 6.0, 'y': 6.0, 'z': 2.0}]
+        
+
+        # Construct piecewise linear path
+        self.path_points = [(wp['x'], wp['y'], wp['z']) for wp in self.waypoints]
+        self.segment_distances = []
+        self.total_path_length = 0.0
+        for i in range(len(self.path_points) - 1):
+            dx = self.path_points[i+1][0] - self.path_points[i][0]
+            dy = self.path_points[i+1][1] - self.path_points[i][1]
+            dz = self.path_points[i+1][2] - self.path_points[i][2]
+            seg_len = math.sqrt(dx*dx + dy*dy + dz*dz)
+            self.segment_distances.append(seg_len)
+            self.total_path_length += seg_len
+
+        # Set final waypoint
+        self.final_waypoint = self.path_points[-1]
+        self.final_x, self.final_y, self.final_z = self.final_waypoint
+
+        # A smaller lookahead for sharper corners
+        self.lookahead_distance = 0.2
+        self.current_path_position = 0.0
+
+        # Control parameters
+        self.kp_pos = 0.2
+        self.kp_yaw = 0.2
+        self.max_lin_vel = 3
+        self.max_alt_vel = 0.5
+        self.max_yaw_vel = 1.8
+        self.position_tolerance = 0.3
+
+        self.previous_px = None
+        self.previous_py = None
+        self.previous_pz = None
+
+        self.ready_timer = self.create_timer(1.0, self.check_drone_ready)
+
+    def waypoint_callback(self, msg):
+
+        for pose in msg.poses:
+            waypoint = {
+            'x': pose.position.x,
+            'y': pose.position.y,
+            'z': pose.position.z,
+            'yaw': 0.0  # Assuming yaw is not provided in PoseArray, set to 0.0 or handle accordingly
+            }
+            self.waypoints.append(waypoint)
+        self.get_logger().info(f'{len(msg.poses)} new waypoints added.')
+
+    def check_drone_ready(self):
+        if self.drone_spawned and self.gt_pose_received:
+            self.ready_timer.cancel()
+            self.start_sequence_timer = self.create_timer(1.0, self.start_sequence)
+        else:
+            self.get_logger().info('Waiting for drone to spawn and initial pose...')
+
+    def start_sequence(self):
+        if self.startup_flag == False:
+            self.startup_flag = True
+            if self.takeOff():
+                self.get_logger().info('Drone takeoff command sent.')
+            else:
+                self.get_logger().info('Drone is already flying.')
+
+            self.posCtrl(False)
+            self.velMode(True)
+            self.get_logger().info('Velocity control mode enabled.')
+
+        if self.waypoints:
+            self.start_sequence_timer.cancel()
+            self.get_logger().info('Following waypoints...')
+            self.timer = self.create_timer(0.1, self.follow_path)
+        else:
+            self.get_logger().info('Waiting for waypoints.')
+
+    def follow_path(self):
+        
+        px = self.gt_pose.position.x
+        py = self.gt_pose.position.y
+        pz = self.gt_pose.position.z
+
+        self.get_logger().info(f"Current Pose: x={px:.2f}, y={py:.2f}, z={pz:.2f}")
+
+        # Check actual distance to the final waypoint
+        dx_final = self.final_x - px
+        dy_final = self.final_y - py
+        dz_final = self.final_z - pz
+        dist_to_final = math.sqrt(dx_final*dx_final + dy_final*dy_final + dz_final*dz_final)
+
+        if self.current_path_position >= self.total_path_length - self.position_tolerance and dist_to_final < 0.3:
+            self.get_logger().info('Reached end of path. Hovering...')
+            self.move(Vector3(), Vector3())  # hover in place
+            self.position_metrics.log_positions()
+            self.position_metrics.save_positions()
+            self.waypoints = []
+            self.action_publisher.publish(String(data="new waypoint"))
+            self.timer.cancel()
+            self.start_sequence_timer = self.create_timer(1.0, self.start_sequence)
+            return
+
+        target_s = min(self.current_path_position + self.lookahead_distance, self.total_path_length)
+        tx, ty, tz = self.get_point_on_path(target_s)
+
+        dx = tx - px
+        dy = ty - py
+        dz = tz - pz
+        dist_to_target = math.sqrt(dx*dx + dy*dy + dz*dz)
+
+        if dist_to_target > 0.05:
+            self.current_path_position += 0.02
+
+        dir_s = min(target_s + 0.1, self.total_path_length)
+        dir_x, dir_y, dir_z = self.get_point_on_path(dir_s)
+        path_dx = dir_x - tx
+        path_dy = dir_y - ty
+        desired_yaw = math.atan2(path_dy, path_dx)
+
+        current_yaw = self.get_current_yaw()
+        yaw_error = desired_yaw - current_yaw
+        yaw_error = math.atan2(math.sin(yaw_error), math.cos(yaw_error))
+
+        local_dx = dx * math.cos(current_yaw) + dy * math.sin(current_yaw)
+        local_dy = -dx * math.sin(current_yaw) + dy * math.cos(current_yaw)
+
+        vx = self.kp_pos * local_dx
+        vy = self.kp_pos * local_dy
+        vz = self.kp_pos * dz
+        vyaw = self.kp_yaw * yaw_error
+
+        # Clamp velocities
+        vx = max(min(vx, self.max_lin_vel), -self.max_lin_vel)
+        vy = max(min(vy, self.max_lin_vel), -self.max_lin_vel)
+        vz = max(min(vz, self.max_alt_vel), -self.max_alt_vel)
+        vyaw = max(min(vyaw, self.max_yaw_vel), -self.max_yaw_vel)
+
+        self.get_logger().info(
+            f"Following path: vx={vx:.3f}, vy={vy:.3f}, vz={vz:.3f}, vyaw={vyaw:.3f}, "
+            f"dist_to_target={dist_to_target:.3f}, remaining={self.total_path_length - self.current_path_position:.3f}, waypoints={len(self.waypoints)}"
+        )
+
+        linear_vec = Vector3(x=vx, y=vy, z=vz)
+        angular_vec = Vector3(z=vyaw)
+        self.move(linear_vec, angular_vec)
+
+
+    def get_point_on_path(self, s):
+        remaining = s
+        for i in range(len(self.path_points) - 1):
+            seg_len = self.segment_distances[i]
+            if remaining <= seg_len:
+                x1, y1, z1 = self.path_points[i]
+                x2, y2, z2 = self.path_points[i+1]
+                ratio = remaining / seg_len
+                x = x1 + ratio * (x2 - x1)
+                y = y1 + ratio * (y2 - y1)
+                z = z1 + ratio * (z2 - z1)
+                return x, y, z
+            else:
+                remaining -= seg_len
+        return self.path_points[-1]
+
+    def get_current_yaw(self):
+        orientation = self.gt_pose.orientation
+        qx, qy, qz, qw = orientation.x, orientation.y, orientation.z, orientation.w
+        siny_cosp = 2.0 * (qw * qz + qx * qy)
+        cosy_cosp = 1.0 - 2.0 * (qy * qy + qz * qz)
+        yaw = math.atan2(siny_cosp, cosy_cosp)
+        return yaw
+
+
+class PositionMetrics():
+    def __init__(self, parent):
+        self.parent = parent
+        self.positions = []
+        
+    def log_positions(self):
+        x, y, z = self.parent.gt_pose.position.x, self.parent.gt_pose.position.y, self.parent.gt_pose.position.z
+        self.positions.append([x, y, z])
+
+    def save_positions(self):
+        name = f"path_{self.parent.waypoints[0]['x']},{self.parent.waypoints[0]['y']})_to_({self.parent.waypoints[-1]['x']},{self.parent.waypoints[-1]['y']}).npy"
+        np.save(name, np.array(self.positions))
+        self.positions = []
+        return
+    
+
+def main(args=None):
+    rclpy.init(args=args)
+    drone_position_control_node = DronePositionControl(namespace='simple_drone')
+    rclpy.spin(drone_position_control_node)
+    drone_position_control_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
