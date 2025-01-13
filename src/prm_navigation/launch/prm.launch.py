8fcdc4edbb00f840a385dd39a919e582881e8138
Martijn Habers <martijn@habers.eu>
2025-01-13 15:15:55 +0100

    add sim time for fast as fock simminggg

--- src/prm_navigation/launch/prm.launch.py
+++ src/prm_navigation/launch/prm.launch.py
@@ -1,22 +1,31 @@
 import os
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
 from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
+
+
 def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time", default="true")
+
     return LaunchDescription([
         Node(
             package='prm_navigation',  # Replace with your package name
             executable='prm_node',  # Replace with your prm_node executable name
             name='prm_node',
             output='screen',
+            parameters=[{"use_sim_time": use_sim_time}]
         ),
         Node(
             package='prm_navigation',  # Replace with your package name
             executable='visualization_node',  # Replace with your visualization_node executable name
             name='visualization_node',
-            output='screen'
+            output='screen',
+            parameters=[{"use_sim_time": use_sim_time}]
         ),
     ])
+    
